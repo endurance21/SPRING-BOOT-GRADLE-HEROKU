197ba9d0f1e9c50177ceb6c4514386e52902e9f1
DIVYANSHU RAJ <divyanshuraj5050@gmail.com>
2022-08-27 11:13:14 +0000

    add rest endpoint

--- src/main/java/com/example/demo/demo/DemoApplication.java
+++ src/main/java/com/example/demo/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +12,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+
+@RestController
+
+class Controller{
+
+	@GetMapping("/")
+	public String handle(){
+		return "Hey, Have a Nice Day" ;
+	}
 }
